6926138c450e8f4b2673552faffe67e142cecd2b
amuhtarov <amuhtarov@akumoproject.com>
2024-01-17 15:29:11 -0600

    test

--- TF-Macie/macie.tf
+++ TF-Macie/macie.tf
@@ -5,7 +5,7 @@ resource "aws_macie2_account" "local" {
 
 resource "aws_macie2_classification_job" "test" {
   job_type = "ONE_TIME"
-  name     = "tejsinngisrgntn"
+  name     = "PII-Detection-${timestamp()}"
   initial_run = true
   sampling_percentage = 100
   s3_job_definition {
@@ -13,6 +13,6 @@ resource "aws_macie2_classification_job" "test" {
       account_id = "${data.aws_caller_identity.current.account_id}"
       buckets    = [aws_s3_bucket.demo.id]
     }
-}
+  }
   depends_on = [aws_macie2_account.local]
 }
