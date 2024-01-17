0c5b0e4ad8f6c1c282ea68abed2b4d6e7b589819
amuhtarov <amuhtarov@akumoproject.com>
2024-01-17 13:44:02 -0600

    test

--- TF-Macie/macie.tf
+++ TF-Macie/macie.tf
@@ -8,7 +8,7 @@ resource "aws_macie2_classification_job" "test" {
   name     = "Testing Macie's PII detection"
   s3_job_definition {
     bucket_definitions {
-      account_id = aws_macie2_account.admin[0].id
+      account_id = "${data.aws_caller_identity.current.account_id}"
       buckets    = [aws_s3_bucket.demo.id]
     }
   }
