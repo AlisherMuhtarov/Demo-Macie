19a11f67ae5a4f5f2dba6d788346f2665a1f4994
alisher muhtarov <therealkimbladi@gmail.com>
2024-01-17 21:42:33 -0600

    test

--- TF-Macie/macie.tf
+++ TF-Macie/macie.tf
@@ -17,11 +17,29 @@ resource "aws_macie2_classification_job" "test" {
   depends_on = [aws_macie2_account.local]
 }
 
-resource "aws_macie2_custom_data_identifier" "example" {
-  name                   = "NAME OF CUSTOM DATA IDENTIFIER"
-  regex                  = "(?i)\\b(John Doe|Jane Smith|123 Main St|456 Oak St|Cityville|Townsville|State|1234-5678-9012-3456|9876-5432-1098-7654)\\b"
+resource "aws_macie2_custom_data_identifier" "address" {
+  name                   = "address_identifier"
+  regex                  = "^\d+\s[A-Za-z0-9\s\.,-]+$"
   description            = "Demo"
-  maximum_match_distance = 10
+  maximum_match_distance = 7
+
+  depends_on = [aws_macie2_account.local]
+}
+
+resource "aws_macie2_custom_data_identifier" "name" {
+  name                   = "name_identifier"
+  regex                  = "^[A-Za-z\s\.'-]+$"
+  description            = "Demo"
+  maximum_match_distance = 4
+
+  depends_on = [aws_macie2_account.local]
+}
+
+resource "aws_macie2_custom_data_identifier" "ccn" {
+  name                   = "ccn_identifier"
+  regex                  = "^\d{4}-?\d{4}-?\d{4}-?\d{4}$"
+  description            = "Demo"
+  maximum_match_distance = 6
 
   depends_on = [aws_macie2_account.local]
 }
