c1759bc2a7b359a58e2f07860ba287b588598681
alisher muhtarov <therealkimbladi@gmail.com>
2024-01-17 23:14:49 -0600

    test

--- TF-Macie/macie.tf
+++ TF-Macie/macie.tf
@@ -8,10 +8,16 @@ resource "aws_macie2_classification_job" "test" {
   name     = "PII-Detection-${timestamp()}"
   initial_run = true
   sampling_percentage = 100
+  custom_data_identifier_ids = ["79ca27ee-de84-4b11-b3a3-18fb42b94a28", "9a56da7e-4075-4ac7-87b5-e673118e9645", "c9e33111-c54d-4bb2-99b4-65c17e4c6901"]
   s3_job_definition {
     bucket_definitions {
       account_id = "${data.aws_caller_identity.current.account_id}"
       buckets    = [aws_s3_bucket.demo.bucket]
+    }
+    bucket_criteria {
+      includes {
+        
+      }
     }
   }
   depends_on = [aws_macie2_account.local]
